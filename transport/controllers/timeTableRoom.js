3b27d9c1f6d9b5d2dc9bd8a27825649b4562e9b3
Utkarsh Saxena <nive54@gmail.com>
2023-12-18 14:21:47 +0530

    added: teacherView method

--- transport/controllers/timeTableRoom.js
+++ transport/controllers/timeTableRoom.js
@@ -1,16 +1,19 @@
-const { generateTimeTableForRoomView, teacherViewTimeTable, getRoomViewByID, getClassViewByID } = require("../../service/week");
-
-
-
+const {
+  generateTimeTableForRoomView,
+  teacherViewTimeTable,
+  getRoomViewByID,
+  getClassViewByID,
+  getTeacherViewByID,
+} = require("../../service/week");
 
 async function getRoomTTView(req, res) {
   try {
-      const roomID = req.params.roomID
-    const generatedTimeTable = await getRoomViewByID(roomID)
-    console.log(generatedTimeTable.length)
-      //console.log(generatedTimeTable)
+    const roomID = req.params.roomID;
+    const generatedTimeTable = await getRoomViewByID(roomID);
+    console.log(generatedTimeTable.length);
+    //console.log(generatedTimeTable)
     if (!generatedTimeTable) {
-      return res.status(401).json({ message: 'no data' });
+      return res.status(401).json({ message: "no data" });
     }
     // //console.log(generatedTimeTable)
     // let tt = []
@@ -18,20 +21,45 @@ async function getRoomTTView(req, res) {
     // // console.log(generatedTimeTable.ttLab)
     // tt.push([...generatedTimeTable.ttRoom])
     // tt.push([...generatedTimeTable.ttLab])
-    res.status(200).json({ "res" : generatedTimeTable });
+    res.status(200).json({ res: generatedTimeTable });
   } catch (error) {
-      res.status(500).json({ message: `Failed to generate TimeTable : ${error}` });
+    res
+      .status(500)
+      .json({ message: `Failed to generate TimeTable : ${error}` });
+  }
+}
+
+async function getTeacherTTView(req, res) {
+  try {
+    const teacherName = req.params.teacherName;
+    const generatedTimeTable = await getTeacherViewByID(teacherName);
+    console.log(generatedTimeTable.length);
+    //console.log(generatedTimeTable)
+    if (!generatedTimeTable) {
+      return res.status(401).json({ message: "no data" });
+    }
+    // //console.log(generatedTimeTable)
+    // let tt = []
+    // // console.log(generatedTimeTable.ttRoom)
+    // // console.log(generatedTimeTable.ttLab)
+    // tt.push([...generatedTimeTable.ttRoom])
+    // tt.push([...generatedTimeTable.ttLab])
+    res.status(200).json({ res: generatedTimeTable });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: `Failed to generate TimeTable : ${error}` });
   }
 }
 
 async function getClassTTView(req, res) {
   try {
-      const className = req.params.className
-    const generatedTimeTable = await getClassViewByID(className)
-    console.log(generatedTimeTable.length)
-      //console.log(generatedTimeTable)
+    const className = req.params.className;
+    const generatedTimeTable = await getClassViewByID(className);
+    console.log(generatedTimeTable.length);
+    //console.log(generatedTimeTable)
     if (!generatedTimeTable) {
-      return res.status(401).json({ message: 'no data' });
+      return res.status(401).json({ message: "no data" });
     }
     // //console.log(generatedTimeTable)
     // let tt = []
@@ -39,45 +67,55 @@ async function getClassTTView(req, res) {
     // // console.log(generatedTimeTable.ttLab)
     // tt.push([...generatedTimeTable.ttRoom])
     // tt.push([...generatedTimeTable.ttLab])
-    res.status(200).json({ "res" : generatedTimeTable });
+    res.status(200).json({ res: generatedTimeTable });
   } catch (error) {
-      res.status(500).json({ message: `Failed to generate TimeTable : ${error}` });
+    res
+      .status(500)
+      .json({ message: `Failed to generate TimeTable : ${error}` });
   }
 }
 
 async function generateTimeTableForRoom(req, res) {
-
   try {
-      const generatedTimeTable = await generateTimeTableForRoomView()
-      //console.log(generatedTimeTable)
+    const generatedTimeTable = await generateTimeTableForRoomView();
+    //console.log(generatedTimeTable)
     if (!generatedTimeTable) {
-      return res.status(401).json({ message: 'no data' });
+      return res.status(401).json({ message: "no data" });
     }
     //console.log(generatedTimeTable)
-    let tt = []
+    let tt = [];
     // console.log(generatedTimeTable.ttRoom)
     // console.log(generatedTimeTable.ttLab)
-    tt.push([...generatedTimeTable.ttRoom])
-    tt.push([...generatedTimeTable.ttLab])
-    res.status(200).json({ "res" : tt });
+    tt.push([...generatedTimeTable.ttRoom]);
+    tt.push([...generatedTimeTable.ttLab]);
+    res.status(200).json({ res: tt });
   } catch (error) {
-      res.status(500).json({ message: `Failed to generate TimeTable : ${error}` });
+    res
+      .status(500)
+      .json({ message: `Failed to generate TimeTable : ${error}` });
   }
 }
 
 async function generateTimeTableForTeacherView(req, res) {
-
   try {
-      const generatedTimeTableTeacherView = await teacherViewTimeTable()
-      //console.log(generatedTimeTable)
+    const generatedTimeTableTeacherView = await teacherViewTimeTable();
+    //console.log(generatedTimeTable)
     if (!generatedTimeTableTeacherView) {
-      return res.status(401).json({ message: 'no data' });
+      return res.status(401).json({ message: "no data" });
     }
-      //console.log(generatedTimeTable)
-      res.status(200).json({ "res" : [...generatedTimeTableTeacherView] });
+    //console.log(generatedTimeTable)
+    res.status(200).json({ res: [...generatedTimeTableTeacherView] });
   } catch (error) {
-      res.status(500).json({ message: `Failed to generate TimeTable : ${error}` });
+    res
+      .status(500)
+      .json({ message: `Failed to generate TimeTable : ${error}` });
   }
 }
 
-module.exports = {generateTimeTableForRoom , generateTimeTableForTeacherView , getRoomTTView , getClassTTView}
+module.exports = {
+  generateTimeTableForRoom,
+  generateTimeTableForTeacherView,
+  getRoomTTView,
+  getClassTTView,
+  getTeacherTTView,
+};
