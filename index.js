e65988350837ae73b9ca1ffb15938493fdaa30f0
Utkarsh Saxena <nive54@gmail.com>
2023-12-21 21:46:00 +0530

    allow cors

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-const express = require('express');
-const dotenv  = require('dotenv');
-const mongoose = require('mongoose');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-const cors = require('cors');
-const router = require('./transport/routes');
-
+const cors = require("cors");
+const router = require("./transport/routes");
 
 const app = express();
 dotenv.config();
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+//app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("welcome to Time Table Maker");
-})
-app.use("/" ,router)
+app.get("/", (req, res) => {
+  res.send("welcome to Time Table Maker");
+});
+app.use("/", router);
 
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true })
-    .then(() =>
-        app.listen(process.env.PORT, () => console.log(`server is running on port ${process.env.PORT}`))
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(`server is running on port ${process.env.PORT}`)
     )
-    .catch(err => console.log(err))
-
+  )
+  .catch((err) => console.log(err));
