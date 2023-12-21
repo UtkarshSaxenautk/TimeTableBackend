18775b34c90d065d6f3ffbd231fab2ad107ba962
Utkarsh Saxena <nive54@gmail.com>
2023-12-21 22:03:27 +0530

    allow cors for frontend app netlify

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://main--scheduler-ptu.netlify.app"
+  );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
